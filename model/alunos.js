bfca051303f439c4bccdb680d1c0573f79db108d
Code0Artuh <87547578+code0Artuh@users.noreply.github.com>
2021-11-24 21:30:35 -0300

    Update alunos.js

--- model/alunos.js
+++ model/alunos.js
@@ -4,8 +4,8 @@ const alunosModel = new mongoose.Schema({
     nome: { type: String, required: true},
     idade: { type: Number, required: true},
     turma: { type: String, required: true},
-    niver: { type: String, required: true},
-    responsavel : { type: String, required: true},
+    niver: { type: String, required: false},
+    responsavel : { type: String, required: false},
     dataCriacao: { type: Date, default: Date.now }
 });
 
